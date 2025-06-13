c48c3b1a7163fa3308a9f3e859b9c4624450fd80
BhavyaYeluri-9395 <bhavya.23bce9395@vitapstudent.ac.in>
2025-06-13 18:56:19 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -4,10 +4,3 @@ function reverseNumber(num) {
     return parseInt(num.toString().split('').reverse().join(''));
 }
 alert("Reversed number is: " + reverseNumber(number));
-
-// Exercise 2
-let inputString = prompt("Enter a string to sort alphabetically:");
-function alphabetOrder(str) {
-    return str.split('').sort().join('');
-}
-alert("Alphabetical order: " + alphabetOrder(inputString));
